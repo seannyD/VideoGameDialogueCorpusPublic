2db28028312af9c63b7a39ee59312fc31f0eeb32
Sean Roberts <seanroberts@Universitys-MacBook-Pro-2.local>
2023-02-03 16:43:29 +0000

    Fixed issue with segmenting text and parsing commented lines

--- processing/parsers/HadesParser2.py
+++ processing/parsers/HadesParser2.py
@@ -4,11 +4,11 @@ import json, re,hjson
 # Extract comment/Cue pairs
 #  Remove commented cue lines ("-- {Cue ")
 #
-# Put boundaries between alternative lines?
-#   Or try to put them in CHOICE structures?
 #
 #  Error: "DisappearFadeLOCATION"
 #  Error: "/VO/ZagreusField_4003"
+#  Error: Commented out cues are still processed (e.g. WeaponData /VO/Thanatos_0300)
+#     Need to filter commented cues and any text comments above
 #
 # Maybe we need to parse by indent level?
 
@@ -24,7 +24,7 @@ import json, re,hjson
 
 
 def parseFile(fileName,parameters={},asJSON=False):
-
+	print(fileName)
 	def cleanLine(txt):
 		if txt.count("\n")>0:
 			txt = txt[:txt.index("\n")]
@@ -33,21 +33,22 @@ def parseFile(fileName,parameters={},asJSON=False):
 		txt = txt.replace("{#PreviousFormat}","")
 		if txt.count("},")>0:
 			txt = txt[:txt.index("},")]
+		txt = txt.replace("--","")
 		txt = txt.replace('"',"").strip()
 		return(txt)
 
-	def bitParser(bit):
+	def bitParser(bit,chunkID=""):
 		#print(bit)
 		#print('-----')
-		txt = bit[bit.index("Text")+4:].strip().replace("=","")		
+		txt = bit[bit.index("Text ")+4:].strip().replace("=","")		
 		
 		bit = bit[bit.index("Cue ="):]
 		charName = "Unknown"
 		if bit.count("/VO/")>0:
 			vo = bit.index("/VO/")+4
 			vx = bit[vo:bit.index(",",vo)].replace('"','').strip()
-			#print("VX:"+vx)
-			vx = vx[:vx.index("_")]
+			if vx.count("_")>0:
+				vx = vx[:vx.index("_")]
 			#print("CN:"+charName)
 			vx = re.findall('[A-Z][^A-Z]*',vx)
 			charName = vx[0]
@@ -57,14 +58,29 @@ def parseFile(fileName,parameters={},asJSON=False):
 			charName = charName.split("_")[1]
 		elif bit.count("Bouldy")>0:
 			charName = "Bouldy"
-			
+		
+		if bit.count("COMMENTED_OUT")>0:
+			charName = "DROPPED_"+charName
+		
 		#print("CN2:",charName)
 		line = {charName:cleanLine(txt)}
+		if len(chunkID)>0:
+			line["_chunk"] = chunkID
 		return(line)
 
 	o = open(fileName)
 	d = o.read()
 	o.close()
+	
+	# Manual edits
+	d = d.replace("\n		-- A_PostBoss01 lines","")
+	#d = d.replace("RequiredFalseTextLines =","RqFTxtL =")
+	
+	# remove notes
+	d = re.sub("\n\s+-- note: .+?\n","\n",d)
+	
+	# Mark commented-out cues:
+	d = re.sub("-- { Cue(.+?)\n", "{ Cue\\1 COMMENTED_OUT\n",d)
 	
 	# Swap order of comment and cue lines
 	d = re.sub("\n(\s*)(--.+?)\n(\s*{ Cue.+?)\n", "\n\\3\n\\1Text = \"\\2\"\n",d)
@@ -81,14 +97,15 @@ def parseFile(fileName,parameters={},asJSON=False):
 	d = re.sub("Binks =\n\t+{","Binks = {",d)
 	d = re.sub("InteractTextLineSets =\n\t+{","InteractTextLineSets = {",d)
 	
+	# Write for debugging
 	open("../data/Hades/Hades/raw/tmp.json",'w').write(d)
 
 	# split into chunks
 	chunks = re.split("([A-Za-z0-9_]+) =\n\t+{",d)
 	chunks = list(zip(chunks[1::2],chunks[2::2]))
-	out = []
+	out = [{"LOCATION": fileName[fileName.rindex("/")+1:].replace(".lua","")}]
 	for chunkID, chunkContents in chunks:
-		
+		#print(chunkID)
 		# Replace "\tEndCue =" with "\t{ Cue ="
 		#  So that end cues are processed
 		chunkContents = chunkContents.replace("\tEndCue =","\t{ Cue =")
@@ -106,9 +123,9 @@ def parseFile(fileName,parameters={},asJSON=False):
 		chunkOut = []
 		for bit in bits:
 			if bit.count('Cue = "')>0 and bit.count('Text = ')>0:
-				print("HERE!")
+				#print("HERE!")
 				inConv = True
-				line = bitParser(bit)
+				line = bitParser(bit,chunkID)
 				conv.append(line)			
 			else:
 				if inConv:

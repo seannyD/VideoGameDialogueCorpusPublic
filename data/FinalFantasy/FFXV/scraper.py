f0d8c52facaab1bd0a9ece457810d22551c7c182
Sean Roberts <seanroberts@Universitys-MacBook-Pro-2.local>
2023-02-05 21:19:45 +0000

    FFXV scraper fix

--- data/FinalFantasy/FFXV/scraper.py
+++ data/FinalFantasy/FFXV/scraper.py
@@ -1,16 +1,28 @@
-from urllib.request import urlopen
+from urllib.request import *
 import time
 
 
 # test: https://ffxiv.gamerescape.com/wiki/A_Final_Temptation#Dialogue
 
+def getPage(url):
+	req = Request(
+		page, 
+		headers={
+			'User-Agent': 'XYZ/3.0'
+		}
+	)
+
+	html = urlopen(req, timeout=10).read().decode('utf-8')
+	return(html)
 
 pages = ["https://thelifestream.net/final-fantasy-xv-lore/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development-part-1/","https://thelifestream.net/final-fantasy-xv-lore/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development-part-2/","https://thelifestream.net/final-fantasy-xv-lore/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development-part-3/","https://thelifestream.net/final-fantasy-xv-lore/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development/final-fantasy-xv-chapter-by-chapter-lore-exposition-and-development-part-4/"]
 pageNum = 0
+print("  downloading pages ...")
 for page in pages:
 	pageNum += 1
-	html = urlopen(page).read().decode('utf-8')
 	print(page)
+	#html = urlopen(page).read().decode('utf-8')
+	html = getPage(page)
 	fileName = "raw/page_"+str(pageNum).zfill(3)+".html"
 	o = open(fileName,'w')
 	o.write(html)

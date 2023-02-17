83e15ef96505339ec07a47a90871830494e11aad
Sean Roberts <seanroberts@v3010-0a830740.wifi.cf.ac.uk>
2023-02-17 11:54:48 +0000

    Add collocation and log likelihood to corpus helper functions

--- analysis/_CorpusHelperFunctions.R
+++ analysis/_CorpusHelperFunctions.R
@@ -78,3 +78,62 @@ loadJSONScripts = function(searchPath = "../data/"){
   return(allData)
 }
 
+
+logLikelihood.G2 = function(a,b,c,d){
+  E1 = c*((a+b) / (c+d))
+  E2 = d*((a+b) / (c+d))
+  G2a = (a*log(a/E1))
+  G2b = (b*log(b/E2))
+  if(is.nan(G2a)){G2a=0}
+  if(is.nan(G2b)){G2b=0}
+  G2 = 2*(G2a + G2b) 
+  return(G2)
+}
+
+logLikelihood.test = 
+  function(freqInCorpus1, freqInCorpus2,
+           sizeOfCorpus1, sizeOfCorpus2,
+           silent=F){
+    G2 = logLikelihood.G2(freqInCorpus1,
+                          freqInCorpus2,
+                          sizeOfCorpus1,
+                          sizeOfCorpus2)
+    c1Rel = freqInCorpus1/sizeOfCorpus1
+    c2Rel = freqInCorpus2/sizeOfCorpus2
+    p.value = pchisq(G2, df=1, lower.tail=FALSE)
+    if(c2Rel>c1Rel){G2=-G2}
+    pres = paste("= ",round(p.value,3))
+    if(p.value<0.0001){pres = "< 0.001"}
+    if(!silent){
+      print(paste("Log Likelihood =",
+                  round(G2,2), ", p ", pres))
+    }
+    return(c(G2, p.value))
+  }
+
+collocation_mutual_information = function(myTokens, target_word, window=c(4,4),minFreq=4){
+  # Keep only words that appear in a 'window' around 
+  # the target (e.g. within 3 words before or 3 words after).
+  toks_target <- tokens_keep(myTokens, pattern = target_word, window = window)
+  # Get the frequency of each word in the windowed data
+  colloc = textstat_frequency(dfm(toks_target))
+  
+  # Work out frequency of all words in whole subcorpus
+  # (not just appearing next to target)
+  totalFreq = textstat_frequency(dfm(myTokens))
+  # Match to colloc 
+  colloc$freqInWholeCorpus = totalFreq[match(colloc$feature,totalFreq$feature),]$frequency
+  # Frequency of the target word
+  targetFrequency = totalFreq[totalFreq$feature==target_word,]$frequency
+  # Total size of corpus
+  numTokensTotal = sum(ntoken(myTokens))
+  # Work out pointwise mutual information
+  colloc$mutualInformation = log((colloc$frequency/numTokensTotal) /
+                                   ((colloc$freqInWholeCorpus/numTokensTotal) * (targetFrequency/numTokensTotal)))
+  # print results, ordered by mutual information
+  colloc = colloc[order(colloc$mutualInformation,decreasing = T),]
+  colloc = colloc[,!names(colloc) %in% c("rank",'group','docfreq')]
+  colloc = colloc[colloc$frequency>=minFreq,]
+  colloc = colloc[colloc$feature!=target_word,]
+  return(colloc)
+}
